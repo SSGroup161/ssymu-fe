117f0b53f663a8c6f84688a8a98cdfcb01672be6
SSGroup161 <shellasaukiag@gmail.com>
2025-03-04 10:30:47 +0700

    change(catalog): png to glb

--- src/components/CardProduct/index.jsx
+++ src/components/CardProduct/index.jsx
@@ -21,20 +21,20 @@ const CardProduct = ({ data }) => {
     return (
         <>
             <div className="w-full max-w-96 flex flex-col items-center justify-start p-2 md:p-6 py-4">
-                <img
+                {/* <img
                     src={data?.poster}
                     alt={data?.title}
                     loading="lazy"
                     className="w-40 h-40 object-cover"
-                />
-                {/* <model-viewer
+                /> */}
+                <model-viewer
                     src={data?.asset_varian1}
                     alt={data?.title}
                     auto-rotate
                     rotation-per-second="30deg"
                     shadow-intensity="1"
                     interaction-prompt="none"
-                ></model-viewer> */}
+                ></model-viewer>
 
                 <div className="w-full flex flex-col items-center justify-center font-montserrat gap-2 md:gap-4">
                     <h1 className="font-semibold text-sm md:text-base text-center">
